1128157c6cc839cdb4e9a25eaef0cfc8f2c444f5
Shawn Ren <shawnren527@gmail.com>
2019-04-03 19:11:48 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,12 @@
 # your code goes here
+class Person
+  attr_accessor :balance, :hapiness_points, :hygiene_points
+  attr_reader :name
+  def initialize(name, balance, hapiness_points, hygiene_points)
+    @name = name
+    @balance = balance
+    @hapiness_points = happiness_points
+    @hygiene_points = hygiene_points
+    
+  end
+end
