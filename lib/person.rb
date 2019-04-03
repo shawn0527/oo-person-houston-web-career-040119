c36e848ff4dfe125a12b5a93512c6c0e2c860a7d
Shawn Ren <shawnren527@gmail.com>
2019-04-03 19:28:27 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -4,11 +4,26 @@ class Person
   attr_accessor :balance, :hapiness_points, :hygiene_points
   attr_reader :name
   binding.pry
-  def initialize(name, balance, hapiness_points, hygiene_points)
+  def initialize(name, balance = 25, hapiness_points = 8, hygiene_points = 8 )
     @name = name
     @balance = balance
     @hapiness_points = happiness_points
     @hygiene_points = hygiene_points
+  end
 
+  def clean?
+    if @hygiene_points > 7
+      return true
+    else
+      false
+    end
+  end
+
+  def happy?
+    if @happiness_points > 7
+      return true
+    else
+      false
+    end
   end
 end
